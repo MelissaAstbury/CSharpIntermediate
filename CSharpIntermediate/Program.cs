13ebecfa59c69dd1bda0e08fe040a605af71e940
Melissa Astbury <melissa.astbury@footasylum.com>
2021-08-10 10:54:24 +0100

    implemented a console application whereby a greeting message is displayed in the termianl

---
+++ CSharpIntermediate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+        public void Introduce(string greeting)
+        {
+            Console.WriteLine($"{greeting}, I am {Name} and I am {Age} year old");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.Name = "Melissa";
+            person.Age = 23;
+            person.Introduce("Good Morning");
+        }
+    }
+}
